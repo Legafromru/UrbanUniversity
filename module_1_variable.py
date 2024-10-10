5f8020cb72819df2c9ce998a410467d9e84124f4
Legafromru <legafromru@gmail.com>
2024-10-10 12:47:14 +0300

    Add files via upload

---
+++ module_1_variable.py
@@ -0,0 +1,5 @@
+zadaniya_vsego=12
+chasov_zatracheno=1.5
+kurs='Python'
+vremya_na_zadanie= (chasov_zatracheno/zadaniya_vsego)
+print ('Курс: ', kurs,', всего задач:', zadaniya_vsego, ', затрачено часов: ', chasov_zatracheno, ', среднее время выполнения: ', vremya_na_zadanie)
