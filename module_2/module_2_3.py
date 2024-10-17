5648ffca141a811300d91eb31a54207f6be49d02
Legafromru <legafromru@gmail.com>
2024-10-17 12:54:27 +0300

    17/10/24

--- module_2/module_2_3.py
+++ module_2/module_2_3.py
@@ -4,11 +4,11 @@
 # не закончится список (выход за границу).
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 _index = 0
-while _index <= len(my_list):
+while _index <= len(my_list) and my_list[_index] >= 0: #или вместо второго условия использовать 11, 12 строки (но так короче)
     if my_list[_index] == 0:
         _index = _index + 1
         continue
-    elif my_list[_index] < 0:
-        break
+    #elif my_list[_index] < 0:
+    #    break
     print(my_list[_index])
     _index = _index + 1
