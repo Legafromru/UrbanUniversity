b1d4988e130425f676a8e6ba8a6f3a1039190c7b
Legafromru <legafromru@gmail.com>
2024-10-17 10:54:32 +0300

    17/10/24

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -5,11 +5,12 @@
     Если все числа равны между собой, то вывести 3
     Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
     Если равных чисел среди 3-х вообще нет, то вывести 0'''
-first=int(input('Введите первое число: '))
-second=int(input('Введите второе число: '))
-third=int(input('Введите третье число: '))
-if first==second and first==third:
-    print('Количество равных чисел -',3)
-elif first==second or first==third:
-    print('Количество равных чисел -',2)
-else:print('Количество равных чисел -',0)
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+if first == second and first == third:
+    print('Количество равных чисел -', 3)
+elif first == second or first == third:
+    print('Количество равных чисел -', 2)
+else:
+    print('Количество равных чисел -', 0)
