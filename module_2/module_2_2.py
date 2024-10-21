1ee00d02878b76445d3b29b40d9f4a84d2f81108
Legafromru <legafromru@gmail.com>
2024-10-21 11:13:08 +0300

    17/10/24

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -10,7 +10,7 @@ second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 if first == second and first == third:
     print('Количество равных чисел -', 3)
-elif first == second or first == third:
+elif first == second or first == third or second==third:
     print('Количество равных чисел -', 2)
 else:
     print('Количество равных чисел -', 0)
