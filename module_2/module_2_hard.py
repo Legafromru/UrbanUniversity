3e392fe4207260126f0fc02e6a8d10b2829aaf87
Legafromru <legafromru@gmail.com>
2024-11-09 12:29:28 +0300

    09/11/24

--- module_2/module_2_hard.py
+++ module_2/module_2_hard.py
@@ -11,12 +11,10 @@
 пароль result, для одного введённого числа.
 11 - число из первой вставки
 11029384756 - нужный пароль (1 и 10, 2 и 9, 3 и 8, 4 и 7, 5 и 6 - пары; число 11 кратно сумме каждой пары)'''
-namber = 1
 result = ''
-while namber < 3 or namber > 20:
-    namber = int(input('Введите число от 3 до 20 '))
+namber = int(input('Введите число от 3 до 20 '))
 for i in range(1, namber):
-    for j in range(i, namber):
+    for j in range(i+1, namber):
         if namber % (i + j) == 0:
             result += str(i) + str(j)
 print('Нужный Вам пароль -', result)
